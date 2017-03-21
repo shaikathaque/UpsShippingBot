555365aa74464916949a9137f72df3405dbc2215
Heather Shapiro <hshapiro93@gmail.com>
2017-03-21 15:50:49 -0400

    added env variables

--- app.js
+++ app.js
@@ -14,10 +14,10 @@ server.listen(process.env.port || process.env.PORT || 3978, function () {
   
 // Create chat bot
 var connector = new builder.ChatConnector({
-    // appId: process.env.MICROSOFT_APP_ID,
-    appId: null,
-    appPassword: null
-    // appPassword: process.env.MICROSOFT_APP_PASSWORD
+    appId: process.env.MICROSOFT_APP_ID,
+    // appId: null,
+    // appPassword: null
+    appPassword: process.env.MICROSOFT_APP_PASSWORD
 });
 var bot = new builder.UniversalBot(connector);
 server.post('/api/messages', connector.listen());
