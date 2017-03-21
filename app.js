95d4100494ce36675fa81f1321636f272c36c427
Heather Shapiro <hshapiro93@gmail.com>
2017-03-21 13:36:14 -0400

    added thumbnail card

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 var restify = require('restify');
 var builder = require('botbuilder');
+var prompts = require('./prompts');
 
 //=========================================================
 // Bot Setup
@@ -31,6 +32,33 @@ var intents = new builder.IntentDialog({recognizers:[recognizer]});
 
 bot.recognizer(recognizer);
 
-bot.dialog('/', function (session) {
+bot.dialog('/', function (session){
     session.send("Hello World");
 });
+
+bot.dialog('start', function(session){
+    session.send("Hi there!");
+    session.beginDialog('help');
+}).triggerAction({matches: /^hello/i})
+
+bot.dialog('help', function(session){
+    session.send(prompts.helpMessage);
+    var msg = new builder.Message(session)
+            .textFormat(builder.TextFormat.xml)
+            .attachments([
+                new builder.ThumbnailCard(session)
+                    .title("Thumbnail Card")
+                    .subtitle("Space Needle")
+                    .text("The <b>Space Needle</b> is an observation tower in Seattle, Washington, a landmark of the Pacific Northwest, and an icon of Seattle.")
+                    // .images([
+                    //     builder.CardImage.create(session, "https://upload.wikimedia.org/wikipedia/commons/thumb/7/7c/Seattlenighttimequeenanne.jpg/320px-Seattlenighttimequeenanne.jpg")
+                    // ])
+                    // .tap(builder.CardAction.openUrl(session, "https://en.wikipedia.org/wiki/Space_Needle"))
+                    .buttons([
+                        builder.CardAction.imBack(session,'select', "select")])
+                        
+            ]);
+    
+    session.send(msg);
+    // return createThumbnailCard(session);
+}).triggerAction({matches: /^help/i})
---
+++ prompts.js
@@ -0,0 +1,3 @@
+module.exports= {
+    helpMessage: 'I can help ship a package. You can choose pick up or dropoff'
+}
