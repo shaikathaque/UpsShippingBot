775c60553f15d1d72ad1cf6470ec71d4f8bc9164
Heather Shapiro <hshapiro93@gmail.com>
2017-03-21 11:29:12 -0400

    added basic bot file

---
+++ app.js
@@ -0,0 +1,36 @@
+var restify = require('restify');
+var builder = require('botbuilder');
+
+//=========================================================
+// Bot Setup
+//=========================================================
+
+// Setup Restify Server
+var server = restify.createServer();
+server.listen(process.env.port || process.env.PORT || 3978, function () {
+   console.log('%s listening to %s', server.name, server.url); 
+});
+  
+// Create chat bot
+var connector = new builder.ChatConnector({
+    // appId: process.env.MICROSOFT_APP_ID,
+    appId: null,
+    appPassword: null
+    // appPassword: process.env.MICROSOFT_APP_PASSWORD
+});
+var bot = new builder.UniversalBot(connector);
+server.post('/api/messages', connector.listen());
+
+//=========================================================
+// Bots Dialogs
+//=========================================================
+
+var model = process.env.model;
+var recognizer = new builder.LuisRecognizer('');
+var intents = new builder.IntentDialog({recognizers:[recognizer]});
+
+bot.recognizer(recognizer);
+
+bot.dialog('/', function (session) {
+    session.send("Hello World");
+});
