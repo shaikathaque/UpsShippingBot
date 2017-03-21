9924f626fce4e5d136d91c02e9874c3c6bd972f1
Heather Shapiro <hshapiro93@gmail.com>
2017-03-21 16:16:48 -0400

    Update app.js

--- app.js
+++ app.js
@@ -37,21 +37,6 @@ bot.dialog('start', function(session){
     session.beginDialog('rootMenu');
 }).triggerAction({matches: /^hello/i})
 
-bot.dialog('help', function(session){
-    session.send(prompts.helpMessage);
-    var msg = new builder.Message(session)
-            .textFormat(builder.TextFormat.xml)
-            .attachments([
-                new builder.ThumbnailCard(session)
-                    .title("Thumbnail Card")
-                    .subtitle("Space Needle")
-                    .text("The <b>Space Needle</b> is an observation tower in Seattle, Washington, a landmark of the Pacific Northwest, and an icon of Seattle.")
-                    // .images([
-                    //     builder.CardImage.create(session, "https://upload.wikimedia.org/wikipedia/commons/thumb/7/7c/Seattlenighttimequeenanne.jpg/320px-Seattlenighttimequeenanne.jpg")
-                    // ])
-                    // .tap(builder.CardAction.openUrl(session, "https://en.wikipedia.org/wiki/Space_Needle"))
-                    .buttons([
-                        builder.CardAction.imBack(session,'shipment', "select")]),
 bot.dialog('rootMenu', [
     function (session) {
         builder.Prompts.choice(session, "Choose an option:", 'Pickup|Dropoff at UPS|Quit',{listStyle:3});
@@ -122,13 +107,6 @@ bot.dialog('quit', function(session){
 //                         builder.CardAction.imBack(session,'dropoff', "dropoff")])
                         
 //             ]);
-    
-
-    session.send(msg);
-    // return createThumbnailCard(session);
-}).triggerAction({matches: /^help/i})
-
-
 bot.dialog('shipment', [function(session, results){
 builder.Prompts.text(session, prompts.shipToMessage);
 },
@@ -144,9 +122,3 @@ else{
 
 }
 ]).triggerAction({matches: /^shipment/i})
-
-
-
-
-//     session.send(msg);
-//     session.endDialog();
