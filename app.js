75cda7fb2cfabfb36a1705bcdfba9c0496a83d23
harshil1793 <harshil1793@gmail.com>
2017-03-22 12:23:12 -0400

    updated dropoff

--- app.js
+++ app.js
@@ -30,10 +30,9 @@ var model = process.env.model;
 var recognizer = new builder.LuisRecognizer('');
 var intents = new builder.IntentDialog({recognizers:[recognizer]});
 
-//Ak2VZoOri8R263-z_IAqqGRcG55S3S5q71H9lSkCsU-1gjnHD1KRUkbeI-zLPp5O
-bot.library(locationDialog.createLibrary("Ak2VZoOri8R263-z_IAqqGRcG55S3S5q71H9lSkCsU-1gjnHD1KRUkbeI-zLPp5O"));
 
 bot.recognizer(recognizer);
+bot.library(locationDialog.createLibrary("Ak2VZoOri8R263-z_IAqqGRcG55S3S5q71H9lSkCsU-1gjnHD1KRUkbeI-zLPp5O"));
 
 bot.dialog('/', function (session){
     session.send("Hello World");
@@ -66,29 +65,11 @@ bot.dialog('help', function(session){
     // return createThumbnailCard(session);
 }).triggerAction({matches: /^help/i})
 
-// Drop off dialog, giving choices to user
-bot.dialog('dropoff', [
-    function(session){
-    builder.Prompts.choice(session,'Would you like to use current location ?', 'Yes|No',{listStyle:3});
-    },
-function(session, results) {
-        switch (results.response.index) {
-            case 0:
-                session.beginDialog('findlocation');
-                break;
-            case 1:
-                session.beginDialog('customlocation');
-                break;
-            default:
-                session.endDialog();
-                break;
-        }
-}]).triggerAction({matches: /^dropoff/i})
 
-// fetch the address using bing Map Api
-bot.dialog('customlocation', [function(session){
+// takes user address and find nearest location
+bot.dialog('dropoff', [function(session){
     locationDialog.getLocation(session, {
-            prompt: "Where should I ship your order?",
+            prompt: "Enter your address",
             useNativeControl: true,
             reverseGeocode: true,
             requiredFields:
@@ -107,21 +88,5 @@ function (session, results) {
             session.send(place.streetAddress + ", " + place.locality + ", " + place.region + ", " + place.country + " (" + place.postalCode + ")");
         }
     }
-]).triggerAction({matches: /^customlocation/i})
+]).triggerAction({matches: /^dropoff/i})
 
-// fetch the current location dialog
-bot.dialog('findlocation', [
-    function (session){
-        builder.Prompts.text(session, "Getting your current location");
-    },
-    function (session) {
-        if(session.message.entities.length != 0){
-            session.userData.lat = session.message.entities[0].geo.latitude;
-            session.userData.lon = session.message.entities[0].geo.longitude;
-            session.send("session.userData.lat");
-            session.endDialog();
-        }else{
-            session.endDialog("Sorry, I didn't get your location.");
-        }
-    }
-]).triggerAction({matches: /^findlocation/i})
